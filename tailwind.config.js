91455b8d97d6eb4de9e199509913a73e7ba8e341
Muhammad Zulkifal <100269142+mzulkifalzk25@users.noreply.github.com>
2024-04-30 01:30:14 +0500

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                pink: {
+                    DEFAULT: '#EF6B82',
+                    '92': 'rgba(239, 107, 130, 0.92)',
+                },
+            },
+        },
+    },
+};
